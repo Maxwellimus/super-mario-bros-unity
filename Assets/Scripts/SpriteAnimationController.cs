f3902bf0eccbc4e5659721015e9d6ffe14cbf862
Max Lampert <mjl0485@gmail.com>
2020-01-20 22:30:57 -0800

    Adding animation controller and setting marios animations (#1)

---
+++ Assets/Scripts/SpriteAnimationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteAnimation[]))]
+public class SpriteAnimationController : MonoBehaviour
+{
+    private SpriteAnimation[] spriteAnimations;
+    private Dictionary<string, SpriteAnimation> spriteAnimationDictionary;
+    private string currentPlayingAnimation;
+
+    void Start()
+    {
+        spriteAnimations = GetComponents<SpriteAnimation>();
+
+        spriteAnimationDictionary = new Dictionary<string, SpriteAnimation>();
+
+        for (int i = 0; i < spriteAnimations.Length; i++)
+        {
+            spriteAnimationDictionary[spriteAnimations[i].animationName] = spriteAnimations[i];
+        }
+    }
+
+    public void PlayAnimation(string name)
+    {
+        if(currentPlayingAnimation != null && name != currentPlayingAnimation)
+        {
+            Debug.Log("Switching animation from " + currentPlayingAnimation + " to " + name);
+            StopCurrentAnimation();
+        }
+
+        if(!spriteAnimationDictionary[name].animationPlaying)
+        {
+            spriteAnimationDictionary[name].PlayAnimation();
+            currentPlayingAnimation = name;
+        }
+    }
+
+    public void StopCurrentAnimation()
+    {
+        spriteAnimationDictionary[currentPlayingAnimation].StopAnimation();
+    }
+}
